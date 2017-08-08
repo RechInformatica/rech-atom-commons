2dd4b562b81e91cebdcfbabd02ae24b9ba4516bc
Jouwee <nicolaspohren@gmail.com>
2017-08-08 09:23:14 -0300

    Criadas rotinas extras na classe AtomEditor. Criada pasta 'view' para permitir criar componentes centralizados. Criado componente para demonstrar itens em um diálogo de seleção

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use babel';
 
 import AtomEditor from './lib/atom/atom-editor';
+import SelectDialog from './lib/atom/view/select-dialog';
 import File from './lib/file';
 import Path from './lib/path';
 
 export default {
   AtomEditor,
   File,
-  Path
+  Path,
+  SelectDialog
 }
--- spec/atom/atom-editor-spec.js
+++ spec/atom/atom-editor-spec.js
@@ -3,17 +3,36 @@
 import AtomEditor from '../../lib/atom/atom-editor';
 
 describe('Path', () => {
-
-  describe('the constructor', () => {
-
+  describe('the setCursor method', () => {
+    let editor;
     beforeEach(() => {
       waitsForPromise(() => {
-        return atom.workspace.open('c.coffee');
+        return atom.workspace.open().then(() => {
+          editor = new AtomEditor();
+          atom.workspace.getActiveTextEditor().insertText("Line 1\nLine 2\nLine 3");
+        });
       });
     });
 
-    it('cannot work with nulls or undefineds', () => {
-      
+    it('ignores undefined', () => {
+      editor.setCursor(1);
+      editor.setCursor(undefined);
+      expect(editor.getCurrentRow()).toBe(1);
+    });
+    it('works passing rows', () => {
+      editor.setCursor(0);
+      expect(editor.getCurrentRow()).toBe(0);
+      editor.setCursor(1);
+      expect(editor.getCurrentRow()).toBe(1);
+      editor.setCursor(2);
+      expect(editor.getCurrentRow()).toBe(2);
+    });
+
+    it('goes to the closest position if the passed position is invalid', () => {
+      editor.setCursor(-1);
+      expect(editor.getCurrentRow()).toBe(0);
+      editor.setCursor(5);
+      expect(editor.getCurrentRow()).toBe(2);
     });
   });
 
